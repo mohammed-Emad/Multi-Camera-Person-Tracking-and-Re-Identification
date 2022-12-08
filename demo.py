d39bc8c097cb911d41037a75f959e87affddcac5
mohammed-Emad <mohammed.imad38@yahoo.com>
2022-12-09 01:38:11 +0200

    color

--- demo.py
+++ demo.py
@@ -425,7 +425,7 @@ def main(yolo):
 
 def get_FrameLabels(frame):
     text_scale = max(1, frame.shape[1] / 1600.)
-    text_thickness = 1 if text_scale > 1.1 else 1
+    text_thickness = 1.5 if text_scale > 1.5 else 1.5
     line_thickness = max(1, int(frame.shape[1] / 500.))
     return text_scale, text_thickness, line_thickness
 
@@ -434,7 +434,7 @@ def cv2_addBox(track_id, frame, x1, y1, x2, y2, line_thickness, text_thickness, 
     color = get_color(abs(track_id))
     cv2.rectangle(frame, (x1, y1), (x2, y2), color=color, thickness=line_thickness)
     cv2.putText(
-        frame, str(track_id), (x1, y1 + 30), cv2.FONT_HERSHEY_PLAIN, text_scale, (0, 0, 255), thickness=text_thickness)
+        frame, str(track_id), (x1, y1 + 30), cv2.FONT_HERSHEY_PLAIN, text_scale, (0, 255, 0), thickness=text_thickness)
 
 
 def write_results(filename, data_type, w_frame_id, w_track_id, w_x1, w_y1, w_x2, w_y2, w_wid, w_hgt):
